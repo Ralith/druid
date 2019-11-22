29bccefda5304f30f9f99b7a4bf511ca156d9167
Colin Rofls <colin@cmyr.net>
2019-11-21 21:55:58 -0500

    [shell] Add Clipboard::available_type_names for debugging

--- druid-shell/src/clipboard.rs
+++ druid-shell/src/clipboard.rs
@@ -160,6 +160,13 @@ impl Clipboard {
     pub fn get_format(&self, format: FormatId) -> Option<Vec<u8>> {
         self.0.get_format(format)
     }
+
+    /// For debugging: print the resolved identifiers for each type currently
+    /// on the clipboard.
+    #[doc(hidden)]
+    pub fn available_type_names(&self) -> Vec<String> {
+        self.0.available_type_names()
+    }
 }
 
 /// A type identifer for the system clipboard.
